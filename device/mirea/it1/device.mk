a14d61866ac0af67834d3a02a00589184d5c9c66
End3rrio <79136744+End3rrio@users.noreply.github.com>
2021-11-19 23:58:54 +0300

    Update device.mk

--- device/mirea/it1/device.mk
+++ device/mirea/it1/device.mk
@@ -1,8 +1,9 @@
-PRODUCT_DEVICE := ocn # кодовое имя устройства
-PRODUCT_NAME := lineage_ocn # кодовое имя продукта, указывается вro.product.name в файле build.prop скомпилированного образа
-PRODUCT_BRAND := HTC # бренд, указывается в ro.product.brand в файлеbuild.prop скомпилированного образа
-PRODUCT_MODEL := HTC U11 # модель, указывается в ro.product.model вфайле build.prop скомпилированного образа
-PRODUCT_MANUFACTURER := HTC # производитель, указывается вro.product.manufacturer в файле build.prop скомпилированного образа
+PRODUCT_DEVICE := it1 # кодовое имя устройства
+PRODUCT_NAME := mirea_it1 # кодовое имя продукта, указывается вro.product.name в файле build.prop скомпилированного образа
+PRODUCT_BRAND := MIREA # бренд, указывается в ro.product.brand в файлеbuild.prop скомпилированного образа
+PRODUCT_MODEL := MIREA IT1 # модель, указывается в ro.product.model вфайле build.prop скомпилированного образа
+PRODUCT_MANUFACTURER := MIREA # производитель, указывается вro.product.manufacturer в файле build.prop скомпилированного образа
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
@@ -25,17 +26,12 @@ PRODUCT_PACKAGES += \
     wmiconfig
     
 PRODUCT_PROPERTY_OVERRIDES := \
-ro.product.first_api_level=25 # переопределение уровня API Android,с которым было выпущено устройство
+ro.product.first_api_level=10 # переопределение уровня API Android,с которым было выпущено устройство
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 ro.secure=0 \ #Отключение режима безопасной загрузки
 
-# Permissions (HTC)
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/com.htc.sensor.edgesense.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.htc.sensor.edgesense.xml \
-    $(LOCAL_PATH)/configs/permissions/com.htc.sensor.hallsensor.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.htc.sensor.hallsensor.xml \
-    $(LOCAL_PATH)/configs/permissions/com.htc.sensor.sensorhub.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.htc.sensor.sensorhub.xml \
-    $(LOCAL_PATH)/configs/permissions/com.htc.software.market.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.htc.software.market.xml
+
     
 # Resolution settings
 PRODUCT_AAPT_CONFIG := normal
